e557f076ab13f77b4c8913741807d0603ad8e8c4
Caroline <carolineyao96@gmail.com>
2017-12-05 11:32:06 -0600

    got all actors but errored during top_actors_by_movie

--- get_data.py
+++ get_data.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import pandas as pd
-from urllib.request import urlretrieve
+from urllib import urlretrieve
 from os.path import isfile
 
 url = 'https://api.themoviedb.org/3/discover/movie?api_key=3b6df7438e6ca12251c94739d6c3d594&language=en-US&page={}&with_keywords=10183'
@@ -85,14 +85,16 @@ def get_actor_popularity(id):
     directors = []
     producers = []
     if isfile('data/movie_actors.csv'):
-        c = pd.read_csv('data/movie_actors.csv',index_col=0)
-        print(c.values[:,0])
-        actors = set(c.values[:,0])
+        c = pd.read_csv('data/movie_actors.csv',index_col=0,header=None)
+        actors = set(c.values[:, 0])
+        # print(actors)
+
     else:
         try:
-            for i in id[:5]:
+            for i in id:
                 success = False
-                while not success:
+                tries = 0
+                while not success and tries < 15:
                     res = requests.get(credits.format(i))
                     print(credits.format(i))
                     jData = json.loads(res.text)
@@ -100,30 +102,73 @@ def get_actor_popularity(id):
                         for j in jData['cast']:
                             actors.add(j['id'])
                         success = True
+                    else:
+                        tries += 1
         finally:
             pd.Series(list(actors)).to_csv('data/movie_actors.csv')
 
-
     popularities = []
-    # if isfile('data/actors.csv'):
-    #     d = pd.read_csv('data/actors.csv',index_col='id')
-        
-    #     actors = actors - set(d.index.values)
+    if isfile('data/actors.csv'):
+        allActPop = pd.read_csv('data/actors.csv')
+    else:
+        try:
+            for i in actors:
+                success = False
+                tries = 0
+                while not success and tries < 15:
+                    res = requests.get(actor_pop.format(i))
+                    print(actor_pop.format(i))
+                    jData = json.loads(res.text)
+                    if not 'status_code' in jData:
+                        popularities.append([i,jData['popularity']])
+                        success = True
+                    else:
+                        tries += 1
+        finally:
+            # print 'popularities'
+            # print popularities
+            allActPop = pd.DataFrame(popularities)
+            allActPop.to_csv('data/actors.csv',index=False)
+            # print(allActPop)
+
+    # get top 3 actors
+    topMovieActors = []
     try:
-        for i in actors:
+        for i in id:
             success = False
-            while not success:
-                res = requests.get(actor_pop.format(i))
-                print(actor_pop.format(i))
+            tries = 0
+            movieActors = []
+            while not success and tries < 15:
+                res = requests.get(credits.format(i))
+                print(credits.format(i))
                 jData = json.loads(res.text)
                 if not 'status_code' in jData:
-                    popularities.append([i,jData['popularity']])
+                    for j in jData['cast']:
+                        movieActors.append(j['id'])    # get all actors in a single movie
                     success = True
-    finally:
-        d = pd.DataFrame(popularities)
-        d.to_csv('data/actors.csv',index=False)
-        print(d)
+                else:
+                    tries += 1
+            if tries >= 15:
+                continue    # skip this movie
+            popList = []
+            for a in movieActors:
+                print a
+                print(allActPop.loc[allActPop['0'] == a].values[:,1])
+                popList.append(allActPop.loc[allActPop['0'] == a].values[:,1][0])
+            # print 'movieActors'
+            # print movieActors
+            # print 'popList'
+            # print popList
 
+            topThree = sorted(zip(popList, movieActors), reverse=True)[:3]
+            # innerArr = [x[1] for x in topThree]
+            innerArr = [i]
+            innerArr.extend([x[1] for x in topThree])
+            # innerArr.append(i)
+            topMovieActors.append(innerArr)
+    finally:
+        df = pd.DataFrame(topMovieActors)
+        df.to_csv('data/top_actors_by_movie.csv',index=False)
 
 if __name__ == "__main__":
     # id = get_id()
